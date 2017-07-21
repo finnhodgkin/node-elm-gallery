d11e76da165895b14d82c6dd0af917141586b6fa
finnhodgkin <finnhodgkin@gmail.com>
2017-07-21 22:57:47 +0100

    adds elm app

---
+++ elm/Main.elm
@@ -0,0 +1,68 @@
+port module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes as HA exposing (..)
+import Html.Events exposing (..)
+import Http exposing (..)
+import Json.Decode exposing (..)
+import Json.Encode exposing (Value)
+import Task exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { singleImage : String
+    , singleThumbs : List String
+    , allThumbs : List String
+    , route : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "" [ "" ] [ "" ] "", Cmd.none )
+
+
+type Msg
+    = UpdateInput String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UpdateInput message ->
+            ( { model | singleImage = "hi.jpg" }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    case model.route of
+        "single" ->
+            single model
+
+        _ ->
+            home model
+
+
+single : Model -> Html Msg
+single model =
+    div [] []
+
+
+home : Model -> Html Msg
+home model =
+    div [] []
+
+
+subscriptions : a -> Sub Msg
+subscriptions model =
+    Sub.none
